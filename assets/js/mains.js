cca92951f48f1770bafa007d4b723a0a8e206b42
Manon Pradier <manonpradier.contact@gmail.com>
2021-05-12 14:38:16 +0200

    bababa

---
+++ assets/js/mains.js
@@ -0,0 +1,31 @@
+$(function() {
+    $( ".drapimg" ).draggable();
+    $( "#dropzone" ).droppable()    
+  });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+$(function(){ 
+var image = new Array ();
+image[1] = "assets/imgs/ciseaux.svg";
+image[2] = "assets/imgs/feuille.svg";
+image[3] = "assets/imgs/pierre.svg";
+
+var size = image.length
+var x = Math.floor(size*Math.random())
+
+$('#randomImages').attr('src',image[x]);
+
+});
+*/
