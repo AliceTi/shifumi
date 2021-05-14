2271eb5217a3fcea37fa3e989cb340959eb6fbc1
Sandryne Avene <nympheale@MacBook-Pro-de-Sandryne.local>
2021-05-14 13:39:17 +0200

    Sand13H39

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -232,4 +232,11 @@ let userAnswer = $("#userAnswer");
 
 
 });
-    
+
+//* Bouton scroll *//
+ $(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
